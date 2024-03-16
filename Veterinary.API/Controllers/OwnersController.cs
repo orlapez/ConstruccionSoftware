660c28f8fd2c0b3080024808a02df9f8134e2e8d
orlapez <orlapez@gmail.com>
2024-03-16 11:54:45 -0500

    Controlller Owners

---
+++ Veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+
+
+
+    [ApiController]
+    [Route("/api/owners")]
+    public class OwnersController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+
+
+
+        public OwnersController(DataContext context)
+        {
+
+
+            _context = context;
+
+
+        }
+
+
+
+        //Método Get por lista--- Select * From Owners
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+
+            return Ok(await _context.Owners.ToListAsync());
+
+
+
+        }
+
+
+
+
+        //Método Get por parámetro--- Select * From Owners
+        [HttpGet("id:int")]
+        public async Task<ActionResult> Get(int id)
+        {
+
+            var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (owner == null)
+            {
+
+
+                return NotFound();
+            }
+
+            return Ok(owner);
+
+
+        }
+
+
+
+
+        //Médoto Post- Guardar registros
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+            _context.Owners.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+
+        }
+
+
+        //Médoto Put- Actualizar registros
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Owner owner)
+        {
+            _context.Owners.Update(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);//200
+
+        }
+
+
+
+        //Método Delete--- Eliminar registros
+        [HttpDelete("id:int")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var filasafectadas = await _context.Owners
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (filasafectadas==0)
+            {
+                return NotFound();//404
+
+
+            }
+            return NoContent(); //204
+             
+
+
+
+        }
+
+
+    }
+}
--- Veterinary.Shared/Entities/Owner.cs
+++ Veterinary.Shared/Entities/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +10,55 @@ namespace Veterinary.Shared.Entities
     public class Owner
     {
         public int Id { get; set; }
+
+
+        [Display(Name = "Primer Nombre")]
+        [Required(ErrorMessage = "Este campo es obliogatorio")]
+        [MaxLength(50, ErrorMessage = "No puede superar los 50 caracteres")]
         public string FirstName { get; set; }
+
+
+        [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "Este campo es obliogatorio")]
+        [MaxLength(50, ErrorMessage = "No puede superar los 50 caracteres")]
         public string LastName { get; set; }
 
-       public string Document { get; set; }
+
+        [Display(Name = "Documento de identidad")]
+        [Required(ErrorMessage = "Este campo es obliogatorio")]
+        [MaxLength(13, ErrorMessage = "No puede superar los 13 dígitos")]
+
+        public string Document { get; set; }
 
 
-        public string Email { get; set; }   
 
+        [Display(Name = "Correo electrónico")]
+        [Required(ErrorMessage = "Este campo es obliogatorio")]
+        [MaxLength(100, ErrorMessage = "No puede superar los 100 caracteres")]
+        [EmailAddress(ErrorMessage ="Email inválido")]
+        public string Email { get; set; }
+
+
+
+        [Display(Name = "Teléfono fijo")]
+      
+        [MaxLength(10, ErrorMessage = "No puede superar los 10 caracteres")]
         public string FixedPhone { get; set; }
 
+
+        [Display(Name = "Teléfono Móvil")]
+
+        [MaxLength(10, ErrorMessage = "No puede superar los 10 caracteres")]
         public string CellPhone { get; set; }
 
 
+        [Display(Name = "Dirección de residencia")]
+
+        [MaxLength(100, ErrorMessage = "No puede superar los 100 caracteres")]
         public string Direccion { get; set; }
 
 
-
-        public string FullName=>$"{FirstName} {LastName}";
+        public string FullName => $"{FirstName} {LastName}";
     }
 
 
@@ -34,5 +66,5 @@ namespace Veterinary.Shared.Entities
 
 
 
-    }
+}
 
